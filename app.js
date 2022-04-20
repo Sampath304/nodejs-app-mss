de706a80764c5de9be9a43653d5efe88e3286356
Sampath304 <101923872+Sampath304@users.noreply.github.com>
2022-04-20 23:20:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -20,8 +20,7 @@ res.send("<h2>Welcome to Node JS express app</h2>"+appEnv.url+appEnv.port+port+p
 console.log(__dirname+"/images/mithunlogo.jpg");
 */
 app.get('/mithuntechnologies', function(request, response) {
-    //response.send("<h2><center>Welcome to Node JS app</h2>");
-    response.write("<h2><center><u>Node JS  Application </u></center></h2>");
+    //response.send("helo");
 	
     response.write("helo" );
     response.end();
